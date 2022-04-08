22dfb8352c235e7355b3c5b75ccbe6b47abfcf07
bybang <joshuaBang26@gmail.com>
2022-04-07 21:44:13 -0400

    Finish Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,21 @@ import React from 'react'
 
 function Header() {
   return (
-    <header>
-      <div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
         <Link href="/">
           <img className="w-44 object-contain cursor-pointer" src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png" alt="" />
         </Link>
-        <div>
+        <div className='hidden md:inline-flex items-center space-x-5'>
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+      </div>
     </header>
   )
 }
